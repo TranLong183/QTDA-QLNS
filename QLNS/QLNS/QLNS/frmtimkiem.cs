a025c4d99fcafd4b7ee1ed63ef4807fdcc417b86
DacHung <dachungchitrung@gmail.com>
2021-03-20 17:39:55 +0700

    add hosothuviec and timkiem function to project

--- QLNS/QLNS/QLNS/frmtimkiem.cs
+++ QLNS/QLNS/QLNS/frmtimkiem.cs
@@ -40,7 +40,34 @@ namespace QLNS
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            try
+            {
+                if ((textBox1.Text == "") || (textBox1.Text == "Nhập vào từ khóa tìm kiếm"))
+                {
+                    MessageBox.Show("bạn chưa nhập tù khóa", "Nhập từ khóa", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+                }
+                else
+                {
+                    if (i == 1)
+                    {
+                        cls.loaddatagridview(dataGridView1, "select * from TblTTNVCoBan where MaNV=N'" + textBox1.Text + "'");
+                    }
+                    if (i == 2)
+                    {
+                        cls.loaddatagridview(dataGridView1, "select * from TblTTNVCoBan where HoTen=N'" + textBox1.Text + "'");
+                    }
+                    if (i == 3)
+                    {
+                        cls.loaddatagridview(dataGridView1, "select * from TblTTNVCoBan where CMTND=N'" + textBox1.Text + "'");
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Tìm kiếm sai");
+            }
+
         }
         
         private void frmtimkiem_Load(object sender, EventArgs e)
@@ -53,17 +80,58 @@ namespace QLNS
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-          
+           
+            
+            try
+            {
+
+                if ((textBox1.Text == "") || (textBox1.Text == "Nhập vào từ khóa tìm kiếm"))
+                {
+                    cls.loaddatagridview(dataGridView1, "select * from TblTTNVCoBan");
+                }
+                else
+                {
+                   
+                    if (i == 1)
+                    {
+                        cls.loatextbox(textBox1, "select * from TblTTNVCoBan where MaNV like N'" + textBox1.Text + "%'", 2);
+                        cls.loaddatagridview(dataGridView1, "select * from TblTTNVCoBan where MaNV like N'" + textBox1.Text + "%'");
+                    }
+                    if (i == 2)
+                    {
+                        cls.loatextbox(textBox1, "select * from TblTTNVCoBan where MaNV like N'" + textBox1.Text + "%'", 3);
+                        cls.loaddatagridview(dataGridView1, "select * from TblTTNVCoBan where HoTen like N'" + textBox1.Text + "%'");
+                    }
+                    if (i == 3)
+                    {
+                        cls.loatextbox(textBox1, "select * from TblTTNVCoBan where MaNV like N'" + textBox1.Text + "%'", 8);
+                        cls.loaddatagridview(dataGridView1, "select * from TblTTNVCoBan where CMTND like N'" + textBox1.Text + "%'");
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("tìm kiếm sai");
+            }
         }
 
         private void textBox1_Click(object sender, EventArgs e)
         {
-            
+            if (textBox1.Text == "Nhập vào từ khóa tìm kiếm")
+            {
+                textBox1.Text = "";
+            }
         }
 
         private void textBox1_KeyPress_1(object sender, KeyPressEventArgs e)
         {
+            //MessageBox.Show("kjlhjkl");
             
+           
+            //if (i == 1)
+            //{
+            //    cls.loaddatagridview(dataGridView1, "select * from TblTTNVCoBan where MaNV like '%'" + textBox1.Text + "'%'");
+            //}
         }
 
         private void button3_MouseMove(object sender, MouseEventArgs e)
